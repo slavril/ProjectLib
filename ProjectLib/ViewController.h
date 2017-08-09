ec151fee438cd95cc7e16118b5cc74ea657afdc2
nguoimaaicungbietlaai <nguoimaaicungbietlaai@aibietlaaidau.ahihi>
2017-08-09 09:30:04 +0700

    Initial Commit

---
+++ ProjectLib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectLib
+//
+//  Created by Son Dang on 8/9/17.
+//  Copyright © 2017 Son Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProjectLib/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ProjectLib
+//
+//  Created by Son Dang on 8/9/17.
+//  Copyright © 2017 Son Dang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
